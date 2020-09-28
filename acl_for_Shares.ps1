1aa3a23e7bc2cec83b92f1d2f3aed97de3f42c76
HubDamian <57452808+HubDamian95@users.noreply.github.com>
2020-09-28 11:51:31 +0100

    Lists ACLs for Shares
    Lists ACLs for Shares

---
+++ acl_for_Shares.ps1
@@ -0,0 +1,16 @@
+﻿start-transcript
+
+$singleDirectory = @("example1","example2"
+)
+
+Foreach($dir in $singleDirectory){
+write-host "$dir"
+$shares = net view \\$dir /all | select -Skip 7 | ?{$_ -match 'disk*'} | %{$_ -match '^(.+?)\s+Disk*'|out-null;$matches[1]}
+Foreach($share in $shares){
+Write-Host "Listing the share for $dir "
+Write-Host "Share mentioned $share"
+get-acl \\$dir\$share | fl
+}
+}
+
+end-trascript
